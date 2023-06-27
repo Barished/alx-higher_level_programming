a227447e357d12699efad303af0be980267f7994
Barished <Shedrackipi@gmail.com>
2023-06-27 20:11:03 +0000

    safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -17,6 +17,6 @@ Otherwise - the result of the call to fct.
     try:
         result = fct(*args)
         return (result)
-    except:
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
