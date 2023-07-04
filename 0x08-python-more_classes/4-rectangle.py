189c66d69b4721037416907c2e55da4b8657f0c7
Barished <Shedrackipi@gmail.com>
2023-07-04 10:23:56 +0000

    Eval is magic updated

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -62,9 +62,9 @@ class Rectangle:
         rect = []
         for i in range(self.__height):
             [rect.append('#') for j in range(self.__width)]
-        if i != self.__height - 1:
-            rect.append("\n")
-            return ("".join(rect))
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
