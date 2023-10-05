7250178fbb828144294f5b29573ca7bf5585791f
Barished <shedrackipi@gmail.com>
2023-10-05 15:46:24 -0700

    network1 urlib

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    b = requests.get(url)
+    if b.status_code >= 400:
+        print("Error code: {}".format(b.status_code))
+    else:
+        print(b.text)
